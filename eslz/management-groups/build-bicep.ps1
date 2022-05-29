6c490cc430ffe860919d308052faa07dd49c6799
Nilesh Patel <nileshpa@microsoft.com>
2022-05-28 21:06:20 -0400

    removed unnecessary ps1 files

--- eslz/management-groups/build-bicep.ps1
+++
@@ -1,6 +0,0 @@
-mkdir build
-$env:path += "C:\Program Files\Bicep CLI"    
-bicep build entlz\management-groups\bicep\main.bicep --outdir build
-Copy-Item entlz\management-groups\main.parameters.*.json build
-Copy-Item entlz\management-groups\deploy-arm.ps1 build
-Copy-Item entlz\management-groups\test-arm.ps1 build
