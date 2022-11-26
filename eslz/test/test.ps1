728fb0a544563823a0a59c6623e52dc6e31d9ec6
Nilesh Patel <nileshpa@microsoft.com>
2022-11-25 22:08:28 -0500

    Added metric alerts for 3 different scopes

--- eslz/test/test.ps1
+++ eslz/test/test.ps1
@@ -6,36 +6,56 @@
         not(contains(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'),'Internet'))        
     ),
         
-        not(
-            or( 
-                ipRangeContains('10.0.0.0/8', field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix')),        
-                ipRangeContains('172.16.0.0/12', field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix')),
-                ipRangeContains('192.168.0.0/16', field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'))
-            )
-        ),
-            
-        'false')
+    not(
+        or( 
+            ipRangeContains('10.0.0.0/8', field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix')),        
+            ipRangeContains('172.16.0.0/12', field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix')),
+            ipRangeContains('192.168.0.0/16', field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'))
+        )
+    ),
+        
+    'false')
 ]
 
 
 
-[if(
+[if
+    (
     
-    and(
-        not(empty(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'))),
-        not(contains(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'),'*')),
-        not(contains(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'),'Internet'))        
-    ),
+        and(
+            not(empty(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'))),
+            not(contains(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'),'*')),
+            not(contains(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'),'Internet'))        
+        ),
         
         not(
-            or( 
-                ipRangeContains(current('allowedIPRanges'), field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'))
-            )
-        ),
-            
-        'false')
+            ipRangeContains(current('allowedIPRanges'), field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'))            
+        ),            
+    
+    'false'
+    )
 ]
 
 
 
 [if(and(not(empty(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'))),not(contains(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'),'*')),not(contains(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'),'Internet'))),not(or(ipRangeContains(current('allowedIPRanges'), field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix')))),'false')]
+
+
+===================================================================================
+
+[if(
+    
+    empty(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix')), bool('true'), 
+    
+    ipRangeContains(current('allowedIPRanges'), if(
+                                                    or(
+        
+                                                        greaterOrEquals(first(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix')), 'a'), 
+                                                        equals(field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'), '*')
+                                                    ), 
+                                                    
+                                                    current('allowedIPRanges'), field('Microsoft.Network/networkSecurityGroups/securityRules/sourceAddressPrefix'))
+    
+    
+    )
+)]
